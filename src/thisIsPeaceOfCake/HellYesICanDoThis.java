f54b43cc94976e7a930ca6555996539d67e5f8b9
mliuzzi69 <117594543+mliuzzi69@users.noreply.github.com>
2022-11-08 22:08:29 -0500

    Update HellYesICanDoThis.java

--- src/thisIsPeaceOfCake/HellYesICanDoThis.java
+++ src/thisIsPeaceOfCake/HellYesICanDoThis.java
@@ -3,15 +3,13 @@ package thisIsPeaceOfCake;
 public class HellYesICanDoThis {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		System.out.println("I learn this");
 		
 	}
 
 	
 	public void thisisFanWhenItWorks() {
-		
 		System.out.println("I will learn it regardless of issues");
+		System.out.println("Ver esto en Eclipse ahora");
 	}
 }
