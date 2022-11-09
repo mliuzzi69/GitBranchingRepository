633d14102c33c434f179da3789c14bc006810805
Marco Liuzzi <liuzzi@metfire.com>
2022-11-08 21:54:08 -0500

    this is on practice branch

--- src/thisIsPeaceOfCake/HellYesICanDoThis.java
+++ src/thisIsPeaceOfCake/HellYesICanDoThis.java
@@ -5,6 +5,13 @@ public class HellYesICanDoThis {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("I learn this");
+		
 	}
 
+	
+	public void thisisFanWhenItWorks() {
+		
+		System.out.println("I will learn it regardless of issues");
+	}
 }
